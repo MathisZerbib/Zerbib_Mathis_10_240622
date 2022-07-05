8eea99fb726b9694af2708d2b301eea03bca67af
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-07-05 18:22:24 +0200

    git init + nav bar + router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import Home from './pages/Home';
+import About from './pages/About';
+import NavBar from './components/Navbar';
 
 
 function App() {
   return (
     <div className="App">
-         <h1>Welcome to React Router!</h1>
+         <NavBar></NavBar>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
@@ -14,35 +18,5 @@ function App() {
   );
 }
 
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>
-          That feels like an existential question, don't you
-          think?
-        </p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-}
 
 export default App;
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,37 @@
+import { NavLink } from 'react-router-dom'
+import styled from 'styled-components'
+import Logo from '../../assets/Logo.png'
+import React from 'react'
+
+const HomeLogo = styled.img`
+  height: 70px;
+`
+
+const NavContainer = styled.nav`
+  padding: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+
+function Header() {
+  return (
+    <NavContainer>
+      <NavLink to="/">
+        <HomeLogo src={Logo} />
+      </NavLink>
+      <div>
+        <NavLink  className={isActive =>
+    "nav-link" + (!isActive ? " unselected" : "")
+  } to="/">Accueil</NavLink>
+        <NavLink className={isActive =>
+    "nav-link" + (!isActive ? " unselected" : "")
+  }  to="/about">A propos</NavLink>
+
+
+      </div>
+    </NavContainer>
+  )
+}
+
+export default Header
