e6db2fafed9213568ef7f46f6c5bb88372f4819a
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-07-06 19:22:11 +0200

    test with fake api

---
+++ src/services/Api/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+// import db from "db.json";
+
+const apiUrl = 'https://mocki.io/v1/c48543ec-cc43-4d71-a2dd-87cb221ece0c'
+export const getLocations = () => {
+  return axios.get(apiUrl);
+}
+
+// fakeApiOnline : https://mocki.io/v1/c48543ec-cc43-4d71-a2dd-87cb221ece0c
