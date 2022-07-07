66c7b0e8ed543bc846d1c38588e1f3f9bba03d0e
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-07-07 15:49:16 +0200

    rating stars + layout location

--- src/pages/Location/index.jsx
+++ src/pages/Location/index.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { getLocationById } from 'services/Api';
-import styled from 'styled-components';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getLocationById } from "services/Api";
+import styled from "styled-components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
+import colors from "utils/style/colors";
 
 const CoverContainer = styled.div`
   height: 415px;
@@ -20,40 +21,74 @@ const CoverImage = styled.img`
   height: inherit;
   object-fit: cover;
   object-position: center;
-  `
-  
+`;
+
+const SectionHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  place-content: space-around;
+`;
+
+const LocTitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  place-content: space-around;
+`;
+
 function Location() {
   const { id } = useParams();
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    getLocationById(id).then(res => setData(res))
-  },[id]);
-  console.log("Data From Location",data)
+    getLocationById(id).then((res) => setData(res));
+  }, [id]);
+  console.log("Data From Location", data);
+  if (data && data.length > 0) {
+    const intRating = parseInt(data[0].rating);
+    console.log("intRating", intRating);
+  }
+
   // do with `userId` what you need to load that user's data
   return (
     <>
-            { data && data.length>0 && data.map((el, i) => {
+      {data &&
+        data.length > 0 &&
+        data.map((el, i) => {
           return (
             <div key={i}>
               <CoverContainer>
                 <CoverImage src={el.cover}></CoverImage>
               </CoverContainer>
-              <p>{el.title}</p>
-              <p>{el.location}</p>
 
-              <FontAwesomeIcon icon={faStar} size="1x" />
-              <FontAwesomeIcon icon={faStar} size="1x" />
-              <FontAwesomeIcon icon={faStar} size="1x" />
-              <FontAwesomeIcon icon={faStar} size="1x" />
-              <FontAwesomeIcon icon={faStar} size="1x" />
-              <p>{el.rating}</p>
-              <p>{el.host.name}</p>
-              <p>{el.tags}</p>
+              <SectionHeaderContainer>
+                <LocTitleContainer>
+                  <p>{el.title}</p>
+                  <p>{el.location}</p>
+                  <p>{el.tags}</p>
+                </LocTitleContainer>
+
+                <div>
+                <p>{el.host.name}</p>
+
+                  {["1", "1", "1", "0", "0"].map((el, i) => { 
+                    if (el === "1") {
+                      return (
+                        <FontAwesomeIcon key={i} icon={faStar} color={colors.primary} />
+                      );
+                    } else {
+                      return (
+                        <FontAwesomeIcon key={i} icon={faStar} color="#E3E3E3" />
+                      );
+                    }
+                  })}
+                </div>
+              </SectionHeaderContainer>
+
               <p>Description</p>
               <p>{el.description}</p>
               <p>{el.equipments}</p>
-
             </div>
           );
         })}
@@ -61,4 +96,4 @@ function Location() {
   );
 }
 
-  export default Location
+export default Location;
