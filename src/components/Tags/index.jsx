b2c494fba4ae8e671a7331a24f1cfe15d9628500
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-08-16 20:19:12 +0200

    tags css + fix tag lenght

--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,21 +1,5 @@
-import styled from "styled-components";
 import React from "react";
-import colors from "utils/style/colors";
 
-const TagList = styled.div`
-padding: 5px 0px;
-margin: 5px 10px 0px 0px;
-border-radius: 10px;
-background-color: ${colors.primary};
-color: #fff;
-`;
-
-
-const Tag = styled.p`
-margin:0  5px;
-flex: 0 0 33.333333%;
-
-`;
 /**
  * @param {{ tags: string[]; }} props
  */
@@ -24,9 +8,9 @@ function Tags(props) {
     <>
       {props.tags.map((tag, index) => {
         return (
-          <TagList key={index}>
-            <Tag>{tag}</Tag>
-          </TagList>
+          <div className="tagList" key={index}>
+            <p className="tag">{tag}</p>
+          </div>
         );
       })}
     </>
