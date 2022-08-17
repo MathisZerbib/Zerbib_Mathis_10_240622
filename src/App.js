e4097b5ba4e82606cd0c41c3435cfbc4dd79101a
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-08-17 13:45:37 +0200

    added overlay + scroll To top

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import{ Home, About, Location, NotFound} from "./pages";
 import { Footer, Navbar} from "./components";
+import ScrollToTop from "utils/style/scrollToTop";
 
 function App() {
   return (
     <div className="App">
       <div className="main-container">
         <Navbar></Navbar>
+        <ScrollToTop>
         <Routes>
           <Route path={"*"} element={<NotFound />} />
           <Route path={"/"} element={<Home />} />
           <Route path={"about"} element={<About />} />
           <Route path={"location/:id"} element={<Location />} />
         </Routes>
+      </ScrollToTop>
       </div>
       <Footer></Footer>
     </div>
